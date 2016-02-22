5f9075dda7f816bb5a78f1d721da837175331962
ToneyAlexander <Alex.Toney11@gmail.com>
2016-02-22 18:14:51 -0500

    Set up performance methods and RPS Testing

--- FEHProteusProject/Util.h
+++ FEHProteusProject/Util.h
@@ -5,11 +5,11 @@
 
 //In order x, y, heading
 //Heading is in degrees, 0 is "North", 90 is "East"
-float* getRPSData(){
-    float data[3];
+//Returns -2 in the dead zone
+void getRPSData(float data[]){
     data[0] = RPS.X();
     data[1] = RPS.Y();
-    data[3] = RPS.Heading();
+    data[2] = RPS.Heading();
 
     //Test the RPS 5 times if there is an error
     if(data[0] == -1){
@@ -27,22 +27,20 @@ float* getRPSData(){
             data[2] = RPS.Heading();
         }
     }
-    return data;
 }
 
 //False is forward, true is backwards
 //In order red, white, blue
-bool* getLeverData(){
-    bool data[3];
+void getLeverData(bool data[]){
     data[0] = RPS.RedSwitchDirection()-1;
     data[1] = RPS.WhiteSwitchDirection()-1;
     data[2] = RPS.BlueSwitchDirection()-1;
-    return data;
 }
 
 void move(float inches, int percent){
     //Get robot location and bearing
-    float start[] = getRPSData();
+    float start[3];
+    getRPSData(start);
     //Reset encoder counts
     rightEnc.ResetCounts();
     leftEnc.ResetCounts();
@@ -55,8 +53,9 @@ void move(float inches, int percent){
     int counts = inches * COUNTS_PER_INCH;
     //Drive the specified number of cycles and or distance
     float delta = 0;
+    float loc[3];
     while((leftEnc.Counts() + rightEnc.Counts()) / 2. < counts && (delta < inches)){
-        float loc[] = getRPSData;
+        getRPSData(loc);
         double x = pow(loc[0] - start[0], 2);
         double y = pow(loc[1] - start[1], 2);
         delta = sqrt(x + y);
@@ -66,7 +65,8 @@ void move(float inches, int percent){
     rightMotor.Stop();
     leftMotor.Stop();
     //Check RPS to see if the distance was correct
-    float end[] = getRPSData();
+    float end[3];
+    getRPSData(end);
     //If the distance and bearing are not within error margins, adjust accordingly
 
 }
@@ -77,23 +77,23 @@ void turn(int angle, int percent){
 void turn_right(int percent, int counts) //using encoders
 {
     //Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    rightEnc.ResetCounts();
+    leftEnc.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-percent);
+    rightMotor.SetPercent(percent);
+    leftMotor.SetPercent(-percent);
 
     //While the average of the left and right encoder are less than counts,
     //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+    while((leftEnc.Counts() + rightEnc.Counts()) / 2. < counts);
 
     //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
+    rightMotor.Stop();
+    leftMotor.Stop();
 }
 
-void turn_left(int percent, int counts) //using encoders
+/*void turn_left(int percent, int counts) //using encoders
 {
     //Reset encoder counts
     right_encoder.ResetCounts();
@@ -110,7 +110,7 @@ void turn_left(int percent, int counts) //using encoders
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
-}
+}*/
 
 void pushArm(float inches){
 
